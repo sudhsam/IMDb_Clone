7d9e226667af8b82ff8c8994f1b485580c7b541d
sudhanshu shubham <shubhamsudhanshu398@gmail.com>
2023-05-11 11:54:41 +0530

    fetch data

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,26 @@
-import { Inter } from "next/font/google";
-const inter = Inter({ subsets: ["latin"] });
-export default function Home() {
-  return <h1>this isv HOME</h1>;
+const API_KEY = process.env.API_KEY;
+import Results from "./../components/Results";
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data"); // this will be caught by the error page and passed to the page as props
+  }
+
+  const data = await res.json();
+
+  const results = data.results;
+  // console.log(results);
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
